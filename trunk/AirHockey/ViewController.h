32f369aae72127ecb836a1d82b961449c0564469
nikidimi <nikidimi@e15ceb5f-e72b-0461-7eb1-01582e75c937>
2012-01-25 20:36:50 +0000

    Gesture recognizer added

--- trunk/AirHockey/ViewController.h
+++ trunk/AirHockey/ViewController.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    
+    IBOutlet UIImageView *secondPlayerMallet;
+    IBOutlet UIImageView *firstPlayerMallet;
+    double firstX;
+    double firstY;
+}
 
 @end
