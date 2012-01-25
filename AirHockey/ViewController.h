7bc1e6d84d84495a88f22bbdfd7a093704e5f38c
nikidimi <nikidimi@e15ceb5f-e72b-0461-7eb1-01582e75c937>
2012-01-25 19:37:18 +0000

    new project added

---
+++ AirHockey/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AirHockey
+//
+//  Created by Nikolay Dimitrov on 1/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
