b9d35aa6181e0a138c213e3925244293b9ad2362
nikidimi <nikidimi@e15ceb5f-e72b-0461-7eb1-01582e75c937>
2012-01-25 20:36:50 +0000

    Gesture recognizer added

--- AirHockey/ViewController.m
+++ AirHockey/ViewController.m
@@ -21,11 +21,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(move:)];
+    [firstPlayerMallet addGestureRecognizer:recognizer];
+    firstPlayerMallet.userInteractionEnabled = YES;
+    
+    
+    
+    recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(move:)];
+    [secondPlayerMallet addGestureRecognizer:recognizer];
+    secondPlayerMallet.userInteractionEnabled = YES;
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void) move:(UIPanGestureRecognizer *) sender{
+    if(sender.state ==UIGestureRecognizerStateBegan){
+        firstX = sender.view.frame.origin.x;
+        firstY = sender.view.frame.origin.y;
+    }
+
+    CGPoint newLocation = [sender translationInView:self.view];
+    sender.view.frame= CGRectMake(firstX+newLocation.x, firstY+ newLocation.y, sender.view.frame.size.width, sender.view.frame.size.height);
+    NSLog(@"Move detected with position %f %f", newLocation.x,newLocation.y);
 }
 
 - (void)viewDidUnload
 {
+    [firstPlayerMallet release];
+    firstPlayerMallet = nil;
+    [secondPlayerMallet release];
+    secondPlayerMallet = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -57,4 +82,9 @@
     return YES;
 }
 
+- (void)dealloc {
+    [firstPlayerMallet release];
+    [secondPlayerMallet release];
+    [super dealloc];
+}
 @end
