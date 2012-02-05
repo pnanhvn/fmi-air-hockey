b1b5dc5201f7546ca15ff33c6e8d8eb598ee4ef5
George.Josifov <George.Josifov@gmail.com>
2012-02-05 14:08:13 +0000

    Added timer for puck move

---
+++ AirHockey/PlayDisk.h
@@ -0,0 +1,19 @@
+//
+//  Puck.h
+//  AirHockey
+//
+//  Created by Seshiro on 2/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayDisk : NSObject
+
+@property CGPoint center;
+@property double angle;
+@property double speed;
+
+//- (void)applicationDidEnterBackground:(UIApplication *)application
+
+@end
