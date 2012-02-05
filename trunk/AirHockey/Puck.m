f9ab1060ab877382f61fa2d81f783f6145a4e6c6
George.Josifov@gmail.com <George.Josifov@gmail.com@e15ceb5f-e72b-0461-7eb1-01582e75c937>
2012-02-05 15:24:14 +0000

    Distance and cos calculated from puck to mallets

--- trunk/AirHockey/Puck.m
+++ trunk/AirHockey/Puck.m
@@ -8,8 +8,22 @@
 
 #import "Puck.h"
 
+
 @implementation Puck
 - (void) checkForCollisionsWithMulletOne: (PlayDisk*) mulletOne andMulletTwo: (PlayDisk*) mulletTwo{
+    double hypotenuseForMalletOne = sqrt(pow(self.center.x - mulletOne.center.x, 2) + pow(self.center.y - mulletOne.center.y, 2));
+    if ((self.radius + mulletOne.radius) <= hypotenuseForMalletOne){
     
+    } 
+    
+    double cosToMalletOne = abs(self.center.x - mulletOne.center.x) / hypotenuseForMalletOne;
+    
+    double hypotenuseForMalletTwo = sqrt(pow(self.center.x - mulletTwo.center.x, 2) + pow(self.center.y - mulletTwo.center.y, 2));
+    
+    if ((self.radius + mulletTwo.radius) <= hypotenuseForMalletTwo){
+        
+    } 
+    
+    double cosToMalletTwo = abs(self.center.x - mulletTwo.center.x) / hypotenuseForMalletTwo;
 }
 @end
