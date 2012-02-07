2fb87a52595faa0f58d37d52786c924acedc72d5
George.Josifov <George.Josifov@gmail.com>
2012-02-07 20:04:26 +0000

    Added view for the players menu

--- AirHockey/AppDelegate.h
+++ AirHockey/AppDelegate.h
@@ -8,12 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@class ViewController;
+//@class ViewController;
+@class PlayerMenuViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) ViewController *viewController;
+@property (strong, nonatomic) PlayerMenuViewController *viewController;
 
 @end
